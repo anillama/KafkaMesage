8e32dcd527bfb6cc0a43dacad31e3a365a6597b9
anillama <33677877+anillama@users.noreply.github.com>
2022-04-28 12:24:40 -0600

    added comment once again

--- src/main/java/com/example/kafkaexample/MessageRequest.java
+++ src/main/java/com/example/kafkaexample/MessageRequest.java
@@ -1,17 +1,17 @@
 package com.example.kafkaexample;
 
 public class MessageRequest {
-
+    //messge
     private String message;
-
+    //messge request
     public MessageRequest(String message) {
         this.message = message;
     }
-
+    //getMessage
     public String getMessage() {
         return message;
     }
-
+    //setMessage
     public void setMessage(String message) {
         this.message = message;
     }
