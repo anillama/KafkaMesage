13465978290fdd327543f10f0835e237eec14024
anillama <33677877+anillama@users.noreply.github.com>
2022-05-13 10:06:03 -0600

    Changing the commit messge

--- src/main/java/com/example/kafkaexample/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/kafkaexample/config/KafkaConsumerConfig.java
@@ -21,8 +21,6 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
     //added one more message on the kafka file
-    //second message
-    //hello
     public Map<String, Object> consumerConfig()
     {
         Map<String, Object> props = new HashMap<>();
